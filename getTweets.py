d98bdc68cbf54a3ff46eda242a6e5fbe8bc9e82a
AbdulH <haritani2002@gmail.com>
2022-05-26 14:52:26 +0200

    added youtube script

--- getTweets.py
+++ getTweets.py
@@ -25,17 +25,29 @@ responseemb = requests.request("GET", urlemb, headers=headersemb, data=payloadem
 parse_jsonemb = json.loads(responseemb.text)
 tweetenb = parse_jsonemb['html']
 
+urlYT = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCiS882YPwZt1NfaM0gR0D9Q&maxResults=10&order=date&type=video&key=AIzaSyDlVaWM4TB8jhSohVEKidDw6Ba9nt_iZ2Q"
+
+payloadYT={}
+headersTY = {}
+
+responseTY = requests.request("GET", urlYT, headers=headersTY, data=payloadYT)
+parse_jsonYT = json.loads(responseTY.text)
+dataYt = parse_jsonYT['items'][0]['id']['videoId']
+print(dataYt)
+
 latestSocialUrl = "https://api.genshinworldrecords.com/latest_socials/3"
 
 latestSocialPayload = json.dumps({
   "tweets": [
     tweetenb
-  ]
+  ],
+  "youtubeVideo": dataYt
 })
 latestSocialHeaders = {
   'Content-Type': 'application/json'
 }
 
 response1 = requests.request("PUT", latestSocialUrl, headers=latestSocialHeaders, data=latestSocialPayload)
+
 print(response1.text)
 print(tweet2)
